2c79c1ee6eccc20b5fa7a9009115d2e6c96300b6
shohag-stack <shohag4y@gmail.com>
2025-08-12 02:32:06 +0600

    table modified

--- src/pages/Tables.jsx
+++ src/pages/Tables.jsx
@@ -13,7 +13,7 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "../components/ui/dialog"
-import Table from '@/components/ui/table';
+import Table from '../components/ui/Table'
 import {
   FileUploadList,
   FileUploadRoot,
