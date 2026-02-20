38808f0ae094ce05621c494b407f72a5141860a3
shohag-stack <shohag4y@gmail.com>
2026-02-21 02:26:13 +0600

    fixed button placement

--- src/components/layout/MainLayout.jsx
+++ src/components/layout/MainLayout.jsx
@@ -1,30 +1,33 @@
-import React from 'react'
-import Sidebar from './Sidebar'
-import Header from './Header';
-import { Outlet, useLocation } from 'react-router-dom';
-import { Box,Grid,GridItem } from '@chakra-ui/react';
-import Greetings from '../ui/Greetings';
+import React from "react";
+import Sidebar from "./Sidebar";
+import Header from "./Header";
+import { Outlet, useLocation } from "react-router-dom";
+import { Box, Grid, GridItem } from "@chakra-ui/react";
+import Greetings from "../ui/Greetings";
 
 export default function MainLayout() {
-    const location = useLocation();
-    const isDashboard = location.pathname === '/';
+  const location = useLocation();
+  const isDashboard = location.pathname === "/";
   return (
     <>
-        <Box w="100%" bg="#F9F9F9" height="100vh" overflow='hidden'>
-            <Grid
-            templateColumns="repeat(6, 1fr)"
-            >
-                <GridItem colSpan={1}>
-                    <Sidebar/>
-                </GridItem>
-                <GridItem colSpan={5} display="flex" flexDirection="column" height="100vh">
-                    {!isDashboard && <Header />}
-                    <Box flex="1" overflowY="auto">
-                        <Outlet />
+      <Box w="100%" bg="#F9F9F9" height="100vh" overflow="hidden">
+        <Grid templateColumns="repeat(6, 1fr)">
+          <GridItem colSpan={1}>
+            <Sidebar />
+          </GridItem>
+          <GridItem
+            colSpan={5}
+            display="flex"
+            flexDirection="column"
+            height="100vh"
+          >
+            {!isDashboard && <Header />}
+            <Box flex="1" overflowY="auto">
+              <Outlet />
             </Box>
-            </GridItem>
-            </Grid>
-        </Box>
+          </GridItem>
+        </Grid>
+      </Box>
     </>
-  )
+  );
 }
--- src/components/layout/Sidebar.jsx
+++ src/components/layout/Sidebar.jsx
@@ -1,14 +1,13 @@
-import React from 'react'
-import { Image,Flex,Box,Heading,VStack} from '@chakra-ui/react'
-import logo from '../../assets/images/logo.svg'
-import DashboardIcons from '../../assets/icons/dashboard.svg'
-import Tables from '../../assets/icons/tables.svg'
-import Menu from '../../assets/icons/menu.svg'
-import { Link } from 'react-router-dom';
-import { NavLink } from 'react-router-dom';
-import Orders from '../../assets//icons/orders.svg'
+import React from "react";
+import { Image, Flex, Box, Heading, VStack, Button } from "@chakra-ui/react";
+import logo from "../../assets/images/logo.svg";
+import DashboardIcons from "../../assets/icons/dashboard.svg";
+import Tables from "../../assets/icons/tables.svg";
+import Menu from "../../assets/icons/menu.svg";
+import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
+import Orders from "../../assets//icons/orders.svg";
 export default function Sidebar() {
-
   // Style for the active link
   const linkStyle = ({ isActive }) => ({
     backgroundColor: isActive ? "#e6e6e6" : "transparent",
@@ -20,51 +19,70 @@ export default function Sidebar() {
     width: "100%",
     padding: "12px 0 12px 20px",
     borderRadius: "10px",
-    transition: "background 0.2s ease-in-out"
-  })
+    transition: "background 0.2s ease-in-out",
+  });
 
   return (
     <>
-      <Box borderRight="1px solid" borderRightColor='#e7e7e7' bg='#fff' height="100vh">
-        <Flex justifyContent="space-between" alignItems='center' padding='10px'>
+      <Box
+        borderRight="1px solid"
+        borderRightColor="#e7e7e7"
+        bg="#fff"
+        height="100vh"
+      >
+        <Flex justifyContent="space-between" alignItems="center" padding="10px">
           <Link to="/">
-            <Image src={logo} alt="logo"/>
+            <Image src={logo} alt="logo" />
           </Link>
           {/* <MdMenu size='2em'/> */}
         </Flex>
 
-        <Box h="1px" bg="#e1e1e1" w="100%" my={4} />  
+        <Box h="1px" bg="#e1e1e1" w="100%" my={4} />
 
-        <Box p={5} gap={5}>
-          <Heading pb={5} size='sm' fontSize='15px' fontWeight="400" color="#6E7784">
+        <Box p={5} pt={1} gap={5}>
+          <Button
+            as={Link}
+            to="/foodMenus"
+            variant="solid"
+            bg={"#F54D2C"}
+            w="100%"
+            mb={5}
+            rounded={"full"}
+          >
+            Order Now
+          </Button>
+          <Heading
+            pb={5}
+            size="sm"
+            fontSize="15px"
+            fontWeight="400"
+            color="#6E7784"
+          >
             MAIN MENU
           </Heading>
-          <VStack alignItems='flex-start' w="100%">
+          <VStack alignItems="flex-start" w="100%">
             <NavLink to="/" style={linkStyle}>
-            <Image src={DashboardIcons} boxSize="20px"/> 
-            Dashboard
-          </NavLink>
+              <Image src={DashboardIcons} boxSize="20px" />
+              Dashboard
+            </NavLink>
 
             <NavLink to="/tables" style={linkStyle}>
-            <Image src={Tables} boxSize="20px"/> 
-            Tables
-          </NavLink>
+              <Image src={Tables} boxSize="20px" />
+              Tables
+            </NavLink>
 
-          <NavLink to="/foodMenus" style={linkStyle}>
-            <Image src={Menu} boxSize="20px"/> 
-            Menu
-          </NavLink>
+            <NavLink to="/foodMenus" style={linkStyle}>
+              <Image src={Menu} boxSize="20px" />
+              Menu
+            </NavLink>
 
-          <NavLink to="/orders" style={linkStyle}>
-            <Image src={Orders} boxSize="20px"/> 
-            Orders
-          </NavLink>
-
+            <NavLink to="/orders" style={linkStyle}>
+              <Image src={Orders} boxSize="20px" />
+              Orders
+            </NavLink>
           </VStack>
         </Box>
-
-
       </Box>
     </>
-  )
+  );
 }
